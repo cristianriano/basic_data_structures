775a5b8175f1465477a2fbd9410843f58c69cf89
Cristian Riaño <cristiancarm11@gmail.com>
2017-03-14 22:01:28 -0500

    Add node sorted in LinkedList

--- LinkedList.java
+++ LinkedList.java
@@ -13,7 +13,7 @@ public class LinkedList{
     while((linea = sc.nextLine()) != null){
       char c = linea.charAt(0);
 
-      if(c == 'a'){ // Add element
+      if(c == 'a'){            // Add element
         int n = new Integer(linea.split(" ")[1]);
         Node newNode = new Node(n);
         newNode.next = null;
@@ -26,7 +26,7 @@ public class LinkedList{
           last = newNode;
         }
       }
-      else if(c == 'p'){ // Print List
+      else if(c == 'p'){      // Print List
         Node tmp = first;
 
         while(tmp != null){
@@ -35,7 +35,7 @@ public class LinkedList{
         }
         System.out.println();
       }
-      else if(c == 'd'){
+      else if(c == 'd'){      // Delete value
         int n = new Integer(linea.split(" ")[1]);
 
         if(first.value == n){
@@ -53,6 +53,36 @@ public class LinkedList{
             }
             previous = tmp;
             tmp = tmp.next;
+          }
+        }
+      }
+      else if(c == 's'){      // Add in sorted mode
+        int n = new Integer(linea.split(" ")[1]);
+        Node newNode = new Node(n);
+
+        if((first == null) && (last == null))
+          first = last = newNode;
+        else{
+          if(newNode.value < first.value){
+            newNode.next = first;
+            first = newNode;
+          }
+          else{
+            boolean added = false;
+
+            Node tmp = first;
+            while(!added){
+              if(tmp.next == null){
+                tmp.next = newNode;
+                added = true;
+              }
+              else if(newNode.value < tmp.next.value){
+                newNode.next = tmp.next;
+                tmp.next = newNode;
+                added = true;
+              }
+              tmp = tmp.next;
+            }
           }
         }
       }
